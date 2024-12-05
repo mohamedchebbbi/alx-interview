d8446ed5e5f590a1f8219feba8621210ce492dc5
mohamedchebbbi <mohamedchebbbi@gmail.com>
2024-12-06 00:55:03 +0100

    ez pz

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,14 +14,13 @@ def island_perimeter(g):
 
     for a in range(rows):
         for b in range(cols):
-            if g[a][b] == 1: #landcell
-                #startwith 4 potential edges
-                perimeter +=4
-                #check topneihhbor
+            if g[a][b] == 1:  # landcell
+                # startwith 4 potential edges
+                perimeter += 4
+                # check topneihhbor
                 if a > 0 and g[a - 1][b] == 1:
                     perimeter -= 2
-                    #check leftneighbor
+                    # check leftneighbor
                     if b > 0 and g[a][b - 1] == 1:
                         perimeter -= 2
     return perimeter
-
